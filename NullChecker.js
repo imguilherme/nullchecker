370eed376d1c488308b47c596520f2aa33bcaeee
Guilherme Costa <guilherme@live.ie>
2018-09-14 16:29:12 -0300

    refactoring nullchecker to accept arrays as nested properties

--- NullChecker.js
+++ NullChecker.js
@@ -4,19 +4,13 @@
 class NullChecker {
 
     static get(path, obj, defaultValue){
-        let splitPath = path.split('.');
-
+        let splitPath = !Array.isArray(path) ?  path.split('.') : path 
         if(splitPath.length > 1 && obj && obj[splitPath[0]]) {
             let newObj =  obj[splitPath[0]];
             splitPath.shift();
             return NullChecker.get(splitPath.join('.'), newObj, defaultValue);
         }
-
-        if(obj[path] !== undefined)
-            return obj[path];
-        else
-            return defaultValue;
-
+        return obj[path] !== undefined ? obj[path] :  defaultValue
     }
 
 }
